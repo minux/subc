8470e8c6df07a6d3431bb47387ef69a8d422484a
minux <minux.ma@gmail.com>
2012-04-26 00:00:00 +0000

    import version 2012-04-26
    Directory structure re-organized.

--- book/p.c
+++ book/p.c
@@ -3,14 +3,17 @@
 #define	MAX 20000
 
 main() {
-	int	i, j, p[MAX], k;
+	int	i, j, p[MAX], k, ref = 0;
 
 	for (k=0, i=3; k<MAX; i+=2) {
-		for (j=0; j<k; j++)
+		for (j=0; j<k; j++) {
+			ref++;
 			if (i % p[j] == 0)
 				break;
+		}
 		if (j < k) continue;
+		ref++;
 		p[k++] = i;
 	}
-	printf("%d\n", p[k-1]);
+	printf("%d %d\n", p[k-1], ref);
 }
